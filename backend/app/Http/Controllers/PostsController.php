3fa6ed31ab33e1d86abcf52f4ee1823eecef9dcd
cawauchi6204 <cawauchi6204@gmail.com>
2020-10-21 20:34:23 +0900

    migrationファイルの作成とマイグレーションの実行完了

---
+++ backend/app/Http/Controllers/PostsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    // $posts = Post::orderby('created_at','desc')->get();
+    // return view('bbs.index',['posts' => $posts]);
+}
